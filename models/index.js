0994ffa895fc8bdc988fb9fc446344a618ee96e2
Chase Malcom <ChaseCMalcom@gmail.com>
2021-06-11 13:16:10 -0400

    added some code to index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const UserModel = require('./user');
 const PlantModel = require('./plant');
-const GardenModel = require('./garden')
+const GardenModel = require('./garden');
+const { DataTypes } = require('sequelize/types');
+
+GardenModel.hasMany(PlantModel, {
+    foreignKey: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        name: Garden_id
+    }
+}); //garden model needs foreign key
+PlantModel.belongsTo(GardenModel)
 
 module.exports = {UserModel, PlantModel, GardenModel}
