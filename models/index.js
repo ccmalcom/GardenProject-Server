6455c29e489d26571ba07020807b7f28ac06e852
Jason smith <jasonsmith@Jasons-MacBook-Pro.local>
2021-06-11 11:47:24 -0400

    delete endpoint

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const UserModel = require('./user');
 const PlantModel = require('./plant');
-const GardenModel = require('./garden')
+//const GardenModel = require('./garden')
 
-module.exports = {UserModel, PlantModel, GardenModel}
+module.exports = {UserModel, PlantModel }//deleted GardenModel
