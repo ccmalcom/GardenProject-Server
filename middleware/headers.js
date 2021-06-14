bbb58e8d75ed5d71367ae6102186a092606e14fc
Jason smith <jasonsmith@Jasons-MacBook-Pro.local>
2021-06-14 16:37:40 -0400

    Accept

--- middleware/headers.js
+++ middleware/headers.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) =>{
 
     res.header('access-control-allow-origin', '*');
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
-    res.header('access-control-allow-headers', 'Origin, X-RequestedWith, Content-Type, accept, Authorization');
+    res.header('access-control-allow-headers', 'Origin, X-RequestedWith, Content-Type, Accept, Authorization');
 
     next()
 }
